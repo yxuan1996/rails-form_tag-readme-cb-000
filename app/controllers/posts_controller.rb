4268328d8ec0325c1f7a1e3d29f0c4cd338a8235
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-20 16:12:58 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,9 @@ class PostsController < ApplicationController
   def new
   end
 
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
