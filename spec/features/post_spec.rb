063885f47f2edf75ca0971c5c81a61210cbd16c9
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-20 16:05:08 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,9 +15,9 @@ describe 'new post' do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
- 
+
     click_on 'Submit Post'
- 
+
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
